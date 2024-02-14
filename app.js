b9c5e3bd851ee7023dd59c848e15bec820380890
adityairkal123 <157140849+adityairkal123@users.noreply.github.com>
2024-02-14 10:25:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Web Developer", "PCB Designer", "Digital System Designer"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelat: 1000,
+    loop: true
+
+})
