eea62721f339b988303976823c0c2648fcdbb81e
adityairkal123 <157140849+adityairkal123@users.noreply.github.com>
2024-09-04 14:36:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Web Developer", "PCB Designer", "Digital System Designer"],
+    strings: ["Web Developer", "PCB Designer", "Digital System Designer",, "Embedded Developer"],
     typeSpeed: 80,
     backSpeed: 80,
     backDelat: 1000,
